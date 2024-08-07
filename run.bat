d9270cf324314e4b7838b860fcd77ac2c22500a4
chinmaykrishnroy <morpheusprefecttt@gmail.com>
2024-08-07 14:59:59 +0530

    improved run.bat to automate creating virtual environment if not created, download necessary packages if not present

--- run.bat
+++ run.bat
@@ -1,4 +1,32 @@
 @echo off
-call .venv\Scripts\activate
+REM Check if Python is installed
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo Python is not installed. Please install Python and try again.
+    timeout /t 3
+    exit /b
+)
+
+REM Check if the venv package is available
+python -c "import venv" >nul 2>&1
+if errorlevel 1 (
+    echo venv package is not available. Installing venv...
+    pip install virtualenv
+    if errorlevel 1 (
+        echo Failed to install virtualenv. Please check your Python and pip installation.
+        timeout /t 3
+        exit /b
+    )
+)
+
+REM Check if the virtual environment exists
+if not exist .venv (
+    echo Creating virtual environment...
+    python -m venv .venv
+)
+
+REM Activate the virtual environment and install packages
+call .venv\Scripts\activate.bat
+pip install PyQt5==5.15.11 yt-dlp==2024.8.6 youtube-search-python==1.6.6
 python main.py
-deactivate
+call .venv\Scripts\deactivate.bat
